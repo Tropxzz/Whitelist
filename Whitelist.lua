ca37b0eb347ee462e63c23db0fbbd271f0747407
Tropxzz <130433432+Tropxzz@users.noreply.github.com>
2023-09-13 20:34:32 +1000

    Create Whitelist.lua

---
+++ Whitelist.lua
@@ -0,0 +1,18 @@
+local E = {}
+
+E.NameWhitelist = {
+	"FIXEDINTERNET";
+	"jopzllwastaken"
+}
+
+E.IDWhitelist = {
+	3686372594;
+	1010372966
+}
+
+E.HWIDWhitelist = {
+	"D269EE3E-81E2-4B22-8F2A-E5747EC5B502";
+	"CEB0A3C3-729D-4EA0-9A93-0CB7F154E305"
+}
+
+return E
